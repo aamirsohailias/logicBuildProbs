f07b71794ef6f6c40031f720ca9bf73c77b40194
Aamir Sohail <amirsohail.leads@gmail.com>
2024-01-24 01:13:27 +0500

    findRandomNum

---
+++ 06_random_num.py
@@ -0,0 +1,14 @@
+# how to generate a random number??
+# we have to import {random library} 1st
+"""
+import random
+print(random.randint(0, 9))
+"""
+
+
+import random
+random.seed(5)
+
+print(random.random())
+print(random.random())
+print(random.random())
